9e95d9261545cd1836ad1654ed28aebf2f6c7f6e
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-06-26 14:07:20 +0200

    Refactoring code for sound managing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+let currentAudio;
+
 /* ** Remet le style par défaut des pads
 * 
 * @param Event event Un événement JS.
 * */
-function removeTransition(event) {console.log(event);
-    document.querySelector('.key[data-key="' + event.keyCode + '"]').classList.remove('playing');
+function removeTransition(event) {
+    if (currentAudio) document.querySelector('.key[data-key="' + event.keyCode + '"]').classList.remove('playing');
 }
 
 /* ** Lit une balise audio en fonction de l'appui sur une touche.
@@ -12,11 +14,19 @@ function removeTransition(event) {console.log(event);
 * */
 function playSound(event) {
     let key = event.keyCode;
-    let currentAudio = (document.querySelector('audio[data-key="' + key + '"]'))?document.querySelector('audio[data-key="' + key + '"]'):null;
 
-    if (currentAudio) currentAudio.play();
+    if (currentAudio) {
+        currentAudio.currentTime = 0;
+        currentAudio.pause();
+    }
 
-    document.querySelector('.key[data-key="' + key + '"]').classList.add('playing');
+    currentAudio = (document.querySelector('audio[data-key="' + key + '"]'))?document.querySelector('audio[data-key="' + key + '"]'):null;
+   
+    if (currentAudio) {
+        currentAudio.play();
+
+        document.querySelector('.key[data-key="' + key + '"]').classList.add('playing');
+    }
 
     // Possibilité d'utiliser l'event "transitionend" pour détecter la fin des transitions CSS.
     //document.querySelector('.key[data-key="' + key + '"]').addEventListener('transitionend', removeTransition);
