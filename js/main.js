34b8efa2b582a05d77c4b648895318ebb282f740
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-06-26 11:06:10 +0200

    Add playSound function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+/* ** Lit une balise audio en fonction de l'appui sur une touche.
+* 
+* @param Int key La clé (dataset.key) du son à jouer.
+* */
+function playSound(key) {
+    document.querySelector('audio[data-key="' + key + '"]').play();
+}
+
 /* ** Sélectionne visuellement la touche préssée dans l'élément .pad.
 * 
 * @param Event event Un événement clavier.
@@ -5,6 +13,8 @@
 function selectKey(event) {
     console.log(event.keyCode);
     document.querySelector('.key[data-key="' + event.keyCode + '"]').style.background = 'black';
+
+    playSound(event.keyCode);
 }
 
 // Document "keydown" listener
